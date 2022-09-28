e45619919fe45f421c75b4eb18812ded0a288367
Andrew Hartpence <andrew.hartpence@gmail.com>
2022-09-28 12:53:10 -0400

    update file path

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 variable template_name                        {} # Relative path and file name to the template file to render
 variable template_vars                        {} # Map of variables which will be substituted into the template
 variable rendered_file_name              {} # Relative path and file name of the final rendered file
-variable rendered_file_destination       {default = "~/"}
 
 data "template_file" "local_template" {
   template = file("./${var.template_name}")
@@ -11,5 +10,6 @@ data "template_file" "local_template" {
 
 resource "local_file" "local_file" {
     content     = data.template_file.local_template.rendered
-    filename    = "${var.rendered_file_destination}/${var.rendered_file_name}"
+    filename    = "${path.module}/${var.rendered_file_name}"
 }
+
